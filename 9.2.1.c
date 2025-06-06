f7df30de1d41c5994d8d3f84feaa132f96c10587
Plygach Viktoriia <xplyhach@stuba.sk>
2025-06-06 14:03:52 +0200

    struktura tridiagonalnej tice

--- 9.2.1.c
+++ 9.2.1.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct{
+   unsigned int size;
+   float *udiag;
+   float *diag;
+   float *ldiag;
+}TDMAT;
+
+float tdmat_determinant(TDMAT *mat);
