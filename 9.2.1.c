0586204e46cab1812575f2b1c107e2e6bd5adae6
Plygach Viktoriia <xplyhach@stuba.sk>
2025-06-07 13:43:46 +0200

    Prototypy funkcii, ktoré sa chystam deklarovat

--- 9.2.1.c
+++ 9.2.1.c
@@ -9,3 +9,10 @@ typedef struct{
 }TDMAT;
 
 float tdmat_determinant(TDMAT *mat);
+
+TDMAT *tdmat_create_with_type(unsigned int size);     //vytvorenie zakladnej datovej struktury TDMAT
+void   tdmat_unit(TDMAT *mat);                        //inicializuje maticu ako jednotkovu tridiagonalnu
+void   tdmat_random(TDMAT *mat);                      //naplni maticu nahodnymi hodnotami v rozsahu (-1, +1)
+void   tdmat_print(const TDMAT *mat);                 //vypise maticu mat na obrazovku
+void   tdmat_destroy(TDMAT *mat);                     //uvolnenie pamati po tridiagonalnej matici reprezentovanej typom TDMAT
+
